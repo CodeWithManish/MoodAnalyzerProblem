4c4200cd069cf2b5855f596e0733a93c67d24ec9
Manish <manishkumar0156@gmail.com>
2022-02-22 12:29:23 +0530

    [Manish] Add: Refator the code to take mood message

--- MoodAnalyzerProblem/MoodAnalyzerProblem/Program.cs
+++ MoodAnalyzerProblem/MoodAnalyzerProblem/Program.cs
@@ -11,6 +11,9 @@ namespace MoodAnalyzerProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Mood Anayzer Program");
+            MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Sad mood");
+            Console.WriteLine("Mood is: " + moodAnalyser.AnalyseMood());
+            Console.ReadLine();
         }
     }
 }
