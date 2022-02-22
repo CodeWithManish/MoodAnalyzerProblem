cbc61d17c1fe0e76aee5b18abcb682c022e7258b
Manish <manishkumar0156@gmail.com>
2022-02-22 12:26:10 +0530

    [Manish] add:Analyse Happy or sad Mood

---
+++ MoodAnalyzerProblem/MoodAnalyzerProblem/MoodAnalyser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzerProblem
+{
+    public class MoodAnalyser
+    {
+        string Message;
+
+        public MoodAnalyser(string Message)
+        {
+            this.Message = Message;
+        }
+        public string AnalyseMood()
+        {
+            if (Message.ToLower().Contains("sad"))
+            {
+                return "sad";
+            }
+            else
+            {
+                return "Happy";
+
+            }
+
+        }
+    }
+}
---
+++ MoodAnalyzerProblem/TestProject1/UnitTest1.cs
@@ -0,0 +1,31 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoodAnalyzerProblem;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        public void TestMethodForSadMood()
+        {
+            string message = "I am in sad Mood";
+            string expected = "sad";
+            MoodAnalyser moodAnalyser = new MoodAnalyser(message);
+            string actual = moodAnalyser.AnalyseMood();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethodForAnyMood()
+        {
+            string message = "I am in Any Mood";
+            string expected = "Happy";
+            MoodAnalyser moodAnalyser = new MoodAnalyser(message);
+            string actual = moodAnalyser.AnalyseMood();
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
